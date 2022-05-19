f40c21a878a1bcb5c2fb97877b89aee33055cb6a
nerpyshka <71436617+ValeriaNigametzianova@users.noreply.github.com>
2022-05-19 23:13:47 +0300

    parkController

--- backend/controllers/parkController.js
+++ backend/controllers/parkController.js
@@ -3,17 +3,28 @@ const {Park} = require("../models/models")
 
 class parkController{
     async create (req, res){
-        const {name,town} = req.body
-        const park = await Park.create({name, town})
-        return res.json(park)
+        try{
+            const {name,town} = req.body
+            const park = await Park.create({name, town})
+            return res.json(park)
+        } catch(e){
+            next(ApiError.badRequest(e.massage))
+        }
 
     }
-    async getAll (req, res){
-        const parks = await Park.findAll()
+    async getAll (req, res, limit, page){
+        page = page || 1
+        limit = limit || 5
+        let offset = page * limit - limit
+        let parks = await Park.findAndCountAll({limit, offset}) //пагинация, выдает кол-во всех полей и записи с указанным лимитом
         return res.json(parks)
     }
     async getOne (req, res){
-        
+        const {id} = req.params
+        const park = await Park.findOne(
+            {where: {id}},
+            include: [{}]
+        )
     }
 }
 
